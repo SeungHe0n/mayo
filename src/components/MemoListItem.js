69bfd1feaf079598947917483c9a2ef6997dd63b
SeungHe0n <014sh@naver.com>
2021-11-11 23:00:26 +0900

    수정버튼 구현

--- src/components/MemoListItem.js
+++ src/components/MemoListItem.js
@@ -1,13 +1,41 @@
-import { MdDelete } from 'react-icons/md';
+import { MdDelete, MdEdit, MdCheckCircle } from 'react-icons/md';
 import cn from 'classnames';
 import './MemoListItem.scss';
+import { useCallback, useState } from 'react';
 
-const MemoListItem = ({ memo, onRemove, onToggle }) => {
+const MemoListItem = ({ memo, onRemove, onToggle, onEdit }) => {
   const { id, text, checked } = memo;
+
+  const [editing, setEditing] = useState({ class: 'edit', icon: <MdEdit /> });
+
+  const onClick = useCallback(
+    (e) => {
+      if (editing.class === 'edit') {
+        setEditing({ class: 'save', icon: <MdCheckCircle /> });
+      } else {
+        setEditing({ class: 'edit', icon: <MdEdit /> });
+      }
+      console.log(editing.class);
+    },
+    [editing],
+  );
+
+  // const onChange = useCallback((e) => {
+  //   setValue(e.target.value);
+  // }, []);
 
   return (
     <div className={cn('MemoListItem', { checked })}>
-      <pre onClick={() => onToggle(id)}>{text}</pre>
+      <pre
+        onClick={() => onToggle(id)}
+        // onChange={onChange}
+        readOnly
+      >
+        {text}
+      </pre>
+      <div className={editing.class} onClick={onClick}>
+        {editing.icon}
+      </div>
       <div className="remove" onClick={() => onRemove(id)}>
         <MdDelete />
       </div>
