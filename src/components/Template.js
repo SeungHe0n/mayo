f898e8b32605fb70ecef11250792b94740aa3d86
SeungHe0n <014sh@naver.com>
2021-12-30 23:59:47 +0900

    styled component 적용 및 버튼 추가

--- src/components/Template.js
+++ src/components/Template.js
@@ -1,11 +1,58 @@
-import './Template.scss';
+import styled from 'styled-components';
+import { MdSearch, MdMenu } from 'react-icons/md';
+
+const Box = styled.div`
+  background: coral;
+  width: 1300px;
+  margin: 0 auto 2rem auto;
+`;
+
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background: yellow;
+  padding: 1rem 1.5rem;
+
+  div {
+    display: flex;
+    align-items: center;
+  }
+
+  // 수정 필요
+  .logo {
+    font-size: 1.5rem;
+  }
+`;
+
+const Button = styled.div`
+  display: flex;
+  font-size: 2rem;
+
+  &:hover {
+    color: red;
+  }
+
+  & + & {
+    margin-left: 1rem;
+  }
+`;
 
 const Template = ({ children }) => {
   return (
-    <div className="Template">
-      <div className="app-title">MAYO</div>
+    <Box>
+      <Header>
+        <div className="logo">MAYO</div>
+        <div>
+          <Button>
+            <MdSearch />
+          </Button>
+          <Button>
+            <MdMenu />
+          </Button>
+        </div>
+      </Header>
       <div>{children}</div>
-    </div>
+    </Box>
   );
 };
 
