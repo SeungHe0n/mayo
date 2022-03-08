6b1a656fd1191dddf5420bfeda840aee8edcb36d
SeungHe0n <014sh@naver.com>
2022-03-08 17:56:32 +0900

    기본 메모 defaultMemo 밖으로 빼냄

--- src/App.js
+++ src/App.js
@@ -5,26 +5,10 @@ import List from './components/List';
 import styled from 'styled-components';
 import Popup from './components/utils/Popup';
 import Footer from './components/Footer';
+import defaultMemo from './defaultMemo';
 
 export default function App() {
-  const [memos, setMemos] = useState([
-    {
-      id: 1,
-      text: '줄바꿈을 사용하고 싶다면\nSHIFT를 누른 상태로 ENTER를 치세요',
-      checked: false,
-    },
-    {
-      id: 2,
-      text: '아이디어를 해결했다면 클릭해서 줄을 그을 수 있어요',
-      checked: true,
-    },
-    {
-      id: 3,
-      text: '위 입력창에 당신의 아이디어나 생각을 입력하세요',
-      checked: false,
-    },
-  ]);
-
+  const [memos, setMemos] = useState(defaultMemo);
   const nextId = useRef(4);
 
   const onInsert = useCallback(
